6ead68c82319182dcda13bdd539e928f38c03eea
Nir Gallner <nir@verisoft.co>
2020-06-25 13:58:20 +0300

    Additional tests

--- src/test/java/course/selenium/GoodMorningTest.java
+++ src/test/java/course/selenium/GoodMorningTest.java
@@ -5,6 +5,10 @@
  */
 package course.selenium;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,6 +19,26 @@ import org.junit.jupiter.api.Test;
  */
 public class GoodMorningTest {
 
+	@BeforeAll
+	public static void beforeAll() {
+		System.out.println("In before all");
+	}
+	
+	@BeforeEach
+	public void beforeEach() {
+		System.out.println("In before each");
+	}
+
+	@AfterEach
+	public void afterEach() {
+		System.out.println("In after each");
+	}
+	
+	@AfterAll
+	public static void afterAll() {
+		System.out.println("In after all");
+	}
+	
 	@Test
 	public void test1() throws InterruptedException {
 		System.out.println("In Test 1");
