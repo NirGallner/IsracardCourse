af46b430fdf4efa55fcc6985ab6c1996a94b22f8
unknown <morguttman@gmail.com>
2020-07-15 20:34:54 +0300

    NewPostTest changes

--- src/test/java/course/selenium/WordPressNewPostTest.java
+++ src/test/java/course/selenium/WordPressNewPostTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -64,33 +65,51 @@ public class WordPressNewPostTest
 	    System.out.println("Header is:" + postPageHeader);
 	    
 	    // Insert header+post
-	    Point publishButton =  driver.findElement(By.id("publish")).getLocation();
-	    System.out.println(publishButton);
-	    driver.findElement(By.id("title")).sendKeys("This is my header");
+	    // Point publishButton =  driver.findElement(By.id("publish")).getLocation();
+	    // System.out.println(publishButton);
+		// Point openDraftWindow = driver.findElement(By.xpath("//div[@id='submitdiv']//span[@class='toggle-indicator']")).getLocation();
+         //System.out.println(openDraftWindow);
+	    
+	    String header = "This is my header" + System.currentTimeMillis();
+	    driver.findElement(By.id("title")).sendKeys(header);
 	    Thread.sleep (5000);
 	    driver.findElement(By.id("content-html")).click();
 	    driver.findElement(By.id("content")).sendKeys("This is my post");
-	    publishButton.move(1420, 433);
-	    driver.findElement(By.id("publish")).click();
-	    Thread.sleep (8000);
-	    System.out.println("Post is published");
-	    
-	    String myPost = driver.findElement(By.id("content")).getText();
-	    System.out.println(myPost);
 	    Thread.sleep (5000);
+	   
+	    
+	    //out of textbox
+
+	     WebElement textBox = driver.findElement(By.id("content"));
+	     textBox.sendKeys(Keys.TAB);
+		 Thread.sleep (3000);
+
+		 driver.findElement(By.id("save-post")).click();
+		 Thread.sleep (3000);
+
+	     //(does not work because name is now + draft)  String myTitle = driver.findElement(By.id("title")).getText();
+	   
           // Back to posts
 	     driver.findElement(By.linkText("All Posts")).click();
-    	//did not work- driver.findElement(By.id("menu-posts")).click();
-	    Thread.sleep (5000);
+    	 //did not work- driver.findElement(By.id("menu-posts")).click();
+	     Thread.sleep (5000);
 	    
          //Confirm post is in list
-		driver.findElement(By.id("post-search-input")).sendKeys(myPost); 
-	    Thread.sleep (5000);
+	     driver.findElement(By.id("post-search-input")).sendKeys(header); 
+	     Thread.sleep (5000);
 
-	    driver.findElement(By.id("search-submit")).click();
-	    
-		boolean allOptionalPosts = driver.findElements(By.className("row-title")).size()!=0;
-		System.out.println(allOptionalPosts);
+	     driver.findElement(By.id("search-submit")).click();
+	     Thread.sleep (5000);
+
+	     boolean postinList = driver.findElements(By.className("row-title")).size()!=0;
+	     System.out.println(postinList);
+	     Thread.sleep (3000);
+
+	     String postStatus = driver.findElement(By.className("post-state")).getText();
+	     assertEquals ("Draft",postStatus);
+	     System.out.println("State is Draft");
+	     Thread.sleep (2000);
+
    	}
     
     @AfterAll
